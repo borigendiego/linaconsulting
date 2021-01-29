5f6dffb56eb37277954e485cdee92b887e090dd4
Juan <jborigen9@gmail.com>
2021-01-29 18:29:04 -0300

    Carousel dimension

--- src/components/deliveryConsulting/Carrousel/index.js
+++ src/components/deliveryConsulting/Carrousel/index.js
@@ -3,6 +3,15 @@ import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 //Css
+import './carousel.scss';
+//Images
+import image1 from '../../../assets/images/deliveryServices1.jpg';
+import image2 from '../../../assets/images/deliveryServices2.jpg';
+import image3 from '../../../assets/images/deliveryServices3.jpg';
+import image4 from '../../../assets/images/deliveryServices4.jpg';
+import image5 from '../../../assets/images/deliveryServices5.jpg';
+import image6 from '../../../assets/images/deliveryServices6.jpeg';
+import image7 from '../../../assets/images/deliveryServices7.png';
 
 const MyCarousel = () => {
     return(
@@ -15,25 +24,25 @@ const MyCarousel = () => {
                 showArrows={true}
             >
                 <div>
-                    <img id={'first'} alt={'first'} />
+                    <img src={image1} id={'first'} alt={'first'} className={'carousel-image'}/>
                 </div>
                 <div>
-                    <img id={'second'} alt={'second'} />
+                    <img src={image2} id={'second'} alt={'second'} className={'carousel-image'} />
                 </div>
                 <div>
-                    <img id={'third'} alt={'third'}  />
+                    <img src={image3} id={'third'} alt={'third'} className={'carousel-image'} />
                 </div>
                 <div>
-                    <img id={'fourth'} alt={'fourth'} />
+                    <img src={image4} id={'fourth'} alt={'fourth'} className={'carousel-image'} />
                 </div>
                 <div>
-                    <img id={'fifth'} alt={'fifth'} />
+                    <img src={image5} id={'fifth'} alt={'fifth'} className={'carousel-image'} />
                 </div>
                 <div>
-                    <img id={'sixth'} alt={'sixth'}  />
+                    <img src={image6} id={'sixth'} alt={'sixth'} className={'carousel-image'} />
                 </div>
                 <div>
-                    <img id={'seventh'} alt={'seventh'} />
+                    <img src={image7} id={'seventh'} alt={'seventh'} className={'carousel-image'} />
                 </div>
             </Carousel>
         </div>
