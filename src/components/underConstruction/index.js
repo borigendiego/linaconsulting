5675b4ee28325bbb7ca14ef2ee80d72867b2f06f
Diego Borigen <diego@iddigital.com.au>
2021-02-10 07:24:52 +1100

    Add under construction page

---
+++ src/components/underConstruction/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ConstructionIcon from '../../assets/icons/construction-icon.png';
+//Style
+import './underConstruction.scss'
+
+const UnderConstructionPage = () => {
+    return (
+        <div className={'uc_container'}>
+            <div className={'uc_content_wrapper'}>
+                <img alt={'underConstruction'} src={ConstructionIcon} />
+                <div className={'text-container'}>
+                    <h1>404</h1>
+                    <h4>Site under construction</h4>
+                    <p>You can reach us here:</p>
+                    <p>kamlesh.lad@linaconsulting.com</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default UnderConstructionPage
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UnderConstruction from './components/underConstruction';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   {/* <App />*/}
+   <UnderConstruction />
   </React.StrictMode>,
   document.getElementById('root')
 );
