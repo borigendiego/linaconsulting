86117ffd9519994b8c0e805944c2cf9136f54fb1
Diego Borigen <diego@iddigital.com.au>
2021-02-14 19:55:40 +1100

    Section layout changes

--- src/components/caseOfStudies/index.js
+++ src/components/caseOfStudies/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-//Styles
-import './caseOfStudies.scss';
 //Components
+import SectionLayout from '../common/SectionLayout/SectionLayout';
 import SimpleCarousel from '../common/carousel';
 //Constants
 import { CAROUSEL_DATA } from './constants';
 
 const CaseOfStudies = () => {
     return (
-        <div className={'cos-wrapper'}>
-            <h2>CASE STUDIES</h2>
-            <h3>Our strategy for your path to success</h3>
+        <SectionLayout sectionId={'#CASE'} sectionTitle={'CASE STUDIES'} sectionSubTitle={'Our strategy for your path to success'}>
             <SimpleCarousel carouselData={CAROUSEL_DATA} />
-        </div>
+        </SectionLayout>
     )
 };
 
