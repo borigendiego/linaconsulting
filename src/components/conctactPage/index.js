9810f9f95303b27fccab6312d145584e51db8847
Diego Borigen <diego@iddigital.com.au>
2021-02-14 20:49:58 +1100

    Banner title fixes

--- src/components/conctactPage/index.js
+++ src/components/conctactPage/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 //Css
 import './contactPage.scss';
 //components
-import Pyramid from '../common/Pyramid/Pyramid';
 import Form from './Form';
 import {contactItem} from '../contact';
 //images
-import logo from '../../assets/images/logo.png';
 import icon1 from '../../assets/images/icon1.png'
 import icon2 from '../../assets/images/icon2.png'
 import icon3 from '../../assets/images/icon3.png'
-
 
 const ContactP = () => {
     return (
@@ -18,7 +15,7 @@ const ContactP = () => {
             <div className={'contact-page-content'}>
                 <div className={'contact-logo-title'}>
                     <h1 className={'contact-title'}>Get</h1>
-                    <h2 className={'contact-title-one'}>in touch</h2>
+                    <h1 className={'contact-title-one'}>in touch</h1>
                 </div>
                 <div className={'icons-text'}>
                     {contactItem(<p className={'contact-page-text'}>Melbourne, Australia</p>, icon1, 'contact-page-item', 'contact-page-img')}
